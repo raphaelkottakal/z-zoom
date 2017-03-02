92423fdcacf73f9d5b74dfca49149a21638c1d08
13308 <varun.bondada@gmail.com>
2017-03-02 15:27:23 +0530

    swipe change and add background image

--- src/App.js
+++ src/App.js
@@ -42,75 +42,84 @@ class App extends Component {
   	const image2P = 2100 + this.state.zoomFactor;
   	const image3P = 1100 + this.state.zoomFactor;
   	const image4P = 400 + this.state.zoomFactor;
+    
 
   	const css = {
   		wrapper: {
-  			perspective: 900,
+  			perspective: 1900,
   			transformStyle: 'preserve-3d',
   			position: 'reletive',
-  			height: 150
+  			height:screen.availHeight
   		},
   		image1: {
-  			right: 0,
-  			position: 'absolute',
-  			transform: 'translateZ(-' + image1P +'px)',
-  			display: (image1P <= 1) ? 'none' : 'block',
+  			right: '-25%',
+        top:'10%', position: 'absolute',
+  			transform: 'translateZ(-' + image1P +'px)', display: (image1P <= 1) ? 'none' : 'block',
   		},
   		image2: {
-  			left: 0,
+  			left: '-8%',
+        top:'10%',
   			position: 'absolute',
   			transform: 'translateZ(-' + image2P +'px)',
   			display: (image2P <= 1) ? 'none' : 'block',
   		},
   		image3: {
-  			right: 0,
+  			right: '-25%',
+        top:'10%',
   			position: 'absolute',
   			transform: 'translateZ(-' + image3P +'px)',
   			display: (image3P <= 1) ? 'none' : 'block',
   		},
   		image4: {
-  			left: 0,
+  			left: '-8%',
+        top:'10%',
   			position: 'absolute',
   			transform: 'translateZ(-' + image4P + 'px)',
   			display: (image4P <= 1) ? 'none' : 'block',
   		},
+      backposition:{
+        backgroundImage:'url(http://assets.myntassets.com/assets/images/lookbook/2017/2/28/11488262826060-bike.jpg)',
+        width:'100%',
+        backgroundSize:'cover',
+        overflow:'hidden',
+        backposition:'fixed',
+        top: 0, 
+        left: 0,
+        minWidth: '100%',
+        minHeight: '100%',
+      }
+
   	}
     return (
-      <div style={{textAlign:'center'}}>
-      	<br />
-      	Swipe left to zoom in. <br />
-      	Swipe right to zoom out.
+
       	<Swipeable
-      		style={{backgroundColor: 'aliceblue', marginTop: 8}}
-      		onSwipingLeft={this.handelTouchZoomIn.bind(this)}
-      		onSwipingRight={this.handelTouchZoomOut.bind(this)}
-      	>
-      	<div style={css.wrapper} ref="wrapper">
-	      		<Imager
-	      			style={css.image1}
-	      			href="http://www.myntra.com/skirts"
-	      			src="http://placehold.it/150x150?text=Skirts"
-	      		/>
-	      		<Imager
-	      			style={css.image2}
-	      			href="http://www.myntra.com/shirts"
-	      			src="http://placehold.it/150x150?text=Shirts"
-	      		/>
-	      		<Imager
-	      			style={css.image3}
-	      			href="http://www.myntra.com/Pants"
-	      			src="http://placehold.it/150x150?text=Pants"
-	      		/>	
-	      		<Imager
-	      			style={css.image4}
-	      			href="http://www.myntra.com/shoes"
-	      			src="http://placehold.it/150x150?text=Shoes"
-	      		/>
-      	</div>
+          style={css.backposition}
+      		onSwipingUp={this.handelTouchZoomIn.bind(this)}
+      		onSwipingDown={this.handelTouchZoomOut.bind(this)}
+      	  >
+        	<div style={css.wrapper} ref="wrapper">
+  	      		<Imager
+  	      			style={css.image1}
+  	      			href="http://www.myntra.com/skirts"
+  	      			src="http://placehold.it/200x300?text=Skirts"
+  	      		/>
+  	      		<Imager
+  	      			style={css.image2}
+  	      			href="http://www.myntra.com/shirts"
+  	      			src="http://placehold.it/200x300?text=Shirts"
+  	      		/>
+  	      		<Imager
+  	      			style={css.image3}
+  	      			href="http://www.myntra.com/Pants"
+  	      			src="http://placehold.it/200x300?text=Pants"
+  	      		/>	
+  	      		<Imager
+  	      			style={css.image4}
+  	      			href="http://www.myntra.com/shoes"
+  	      			src="http://placehold.it/200x300?text=Shoes"
+  	      		/>
+        	</div>
       	</Swipeable>
-      	<br />
-      	Tap on section to shop.
-      </div>
     );
   }
 }
